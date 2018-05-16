439cd32f04d642b39081fbd83fad8a8b91779bcd
amalshehu <amalshehu@gmail.com>
2018-05-16 23:10:03 +0530

    Append timestamp with file watcher update

--- src/util.mjs
+++ src/util.mjs
@@ -1,8 +1,15 @@
 export const mapToJson = data => {
-    return JSON.stringify(
-        [...data].map(([key, obj]) => ({
-            key,
-            ...obj
-        }))
-    )
+  return JSON.stringify(
+    [...data].map(([key, obj]) => ({
+      key,
+      ...obj
+    }))
+  )
 }
+
+export const logWithTime = message => {
+  return console.log(`${new Date().toUTCString()}: ${message}`)
+}
+// fs.watch(this.dirName, (event, fileName) => {
+//     logWithTime(`${event} ${fileName}`)
+// })

5fcf77e17488735e200a5fd2131f8d38a2abe25a
amalshehu <amalshehu@gmail.com>
2018-05-18 11:06:41 +0530

    Add load balancer

--- src/server.mjs
+++ src/server.mjs
@@ -1,50 +1,66 @@
 import http from 'http'
+import cluster from 'cluster'
+import os from 'os'
+import { mapToJson, log } from './util'
 import Storage from './storage'
-import { mapToJson } from './util'
 
 const database = new Storage()
-// Server
-const server = http.createServer()
-server.on('request', (req, res) => {
-  switch (req.url) {
-    case '/':
-      res.writeHead(200) // 200: OK
-      res.end()
-      break
-    case '/set':
-      if (req.method === 'POST') {
-        let body = ''
-        req.on('data', chunk => {
-          body += chunk.toString() // convert Buffer to string
-        })
-        req.on('end', () => {
-          const data = JSON.parse(body)
-          const value = Object.keys(data).map(i => [i, data[i]])
-          res.writeHead(201) // 201: CREATED
-          database.setItem(...value[0]).then(result => {
-            // console.log('res', result)
-          })
-          res.end('Created #4k3hhjg45kqtj67')
-        })
-      }
-      break
-    case '/get':
-      res.writeHead(200, {
-        // 200: SUCCESS
-        'Content-Type': 'application/json'
-      })
-      res.end(mapToJson(database.DB))
-      break
-    case '/delete':
-      res.writeHead(204) // 204: NO CONTENT
-      res.end()
-      break
-    default:
-      res.writeHead(404) // 404: NOT FOUND
-      res.end()
-  }
-})
-server.listen(8000)
+const numCPUs = os.cpus().length
 
-// Print URL for accessing server
-console.log('Server running at http://127.0.0.1:8000/')
+if (cluster.isMaster) {
+  log(`Master ${process.pid} is running`, 'mg')
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork()
+  }
+  cluster.on('exit', (worker, code, signal) => {
+    log(`worker ${worker.process.pid} died`, 'r')
+  })
+} else {
+  // Workers can share any TCP connection
+  // In this case it is an HTTP server
+  const server = http.createServer()
+  server.on('request', (req, res) => {
+    switch (req.url) {
+      case '/':
+        res.writeHead(200) // 200: OK
+        res.end()
+        break
+      case '/set':
+        if (req.method === 'POST') {
+          let body = ''
+          req.on('data', chunk => {
+            body += chunk.toString() // Convert buffer to string
+          })
+          req.on('end', () => {
+            const data = JSON.parse(body)
+            const value = Object.keys(data).map(i => [i, data[i]])
+            res.writeHead(201) // 201: CREATED
+            database.setItem(...value[0]).then(result => {
+              // console.log('res', result)
+            })
+            res.end('INSERT e0d123e5f316bef78bfdf5a008837577 OK') // Mock
+          })
+        }
+        break
+      case '/get':
+        res.writeHead(200, {
+          // 200: SUCCESS
+          'Content-Type': 'application/json'
+        })
+        res.end(mapToJson(database.DB))
+        break
+      case '/delete':
+        res.writeHead(204) // 204: NO CONTENT
+        res.end()
+        break
+      default:
+        res.writeHead(404) // 404: NOT FOUND
+        res.end()
+    }
+  })
+  server.listen(8000)
+
+  // Print URL for accessing server
+  log('Server is listening on http://127.0.0.1:8000/', 'g')
+  log(`Worker ${process.pid} started`, 'y')
+}

a8c40d7126a7ba235b5c9fe7575f3315a8fdd501
amalshehu <amalshehu@gmail.com>
2018-05-18 11:06:52 +0530

    Add custom logger

--- src/util.mjs
+++ src/util.mjs
@@ -10,6 +10,20 @@ export const mapToJson = data => {
 export const logWithTime = message => {
   return console.log(`${new Date().toUTCString()}: ${message}`)
 }
+export const log = (message, code) => {
+  const colors = {
+    r: '\x1b[41m',
+    y: '\x1b[36m',
+    g: '\x1b[32m',
+    mg: '\x1b[31m',
+    b: '\x1b[33m'
+  }
+  const date = new Date()
+  const formatTime = `${date.getHours()}:${(date.getMinutes() < 10 ? '0' : '') +
+    date.getMinutes()}:${date.getSeconds()}`
+  console.log(`${colors[code]}%s\x1b[0m`, `${formatTime}: ${message}`)
+}
+
 // fs.watch(this.dirName, (event, fileName) => {
 //     logWithTime(`${event} ${fileName}`)
 // })
